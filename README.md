2561279ef5aecc66c70b318aa8031f7b0faaeb5e
Lovesh <lovesh.harchandani@evernym.com>
2017-11-26 00:40:58 +0530

    add apps menu

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # ed25519 keygen using Electron
 
-This is a my first Electron application based on some electron sample code i copied from few github repos, it is a keygen and used to generate ed25519 keypair.
+This is a my first Electron application based on some electron sample code i copied from few github repos, it is a keygen and used to generate ed25519 keypair. Also there is an `Apps` menu to load a window that can be used to create hashes.
 I used linux for development. It can create linux and windows apps using `npm run package-linux` and `npm run package-win` respectively, you need wine installed for creating windows app though
 
 # Next steps
--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 const electron = require('electron')
 // Module to control application life.
-const app = electron.app
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+
+const {
+  app,
+  BrowserWindow,
+  Menu,
+  MenuItem
+} = electron;
 
 const path = require('path')
 const url = require('url')
@@ -11,67 +15,36 @@ const url = require('url')
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
-function createHelloWorldWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
 
+function loadHelloWorldWindow() {
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
     slashes: true
   }))
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
 }
 
-function createHashWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    // Set the initial width to 800px
-    width: 800,
-    // Set the initial height to 600px
-    height: 600,
-    // Set the default background color of the window to match the CSS
-    // background color of the page, this prevents any white flickering
-    backgroundColor: "#D6D8DC",
-    // Don't show the window until it's ready, this prevents any white flickering
-    show: false
-  })
 
+function loadHashWindow() {
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'hash.html'),
     protocol: 'file:',
     slashes: true
   }))
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
-
-  mainWindow.once('ready-to-show', () => {
-    mainWindow.show()
-  })
 }
 
-function createKeygenWindow () {
-  // Create the browser window.
+function loadKeygenWindow() {
+  // and load the index.html of the app.
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'keygen.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+}
+
+function createMainWindow() {
   mainWindow = new BrowserWindow({
     // Set the initial width to 800px
     width: 800,
@@ -84,12 +57,8 @@ function createKeygenWindow () {
     show: false
   })
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'keygen.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+  loadKeygenWindow();
+
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 
@@ -104,10 +73,48 @@ function createKeygenWindow () {
   mainWindow.once('ready-to-show', () => {
     mainWindow.show()
   })
+
+  addAppMenu();
 }
 
+const appMenuTemplate = {
+  label: 'Apps',
+  visible: true,
+  enabled: true,
+  submenu: [{
+      label: 'Hello World',
+      click() {
+        console.log('Hello World');
+        loadHelloWorldWindow();
+      }
+    },
+    {
+      label: 'Hash',
+      click() {
+        console.log('Hash');
+        loadHashWindow();
+      }
+    },
+    {
+      label: 'Keygen',
+      click() {
+        console.log('Keygen');
+        loadKeygenWindow();
+      }
+    }
+  ]
+};
+
+function addAppMenu() {
+  const appMenu = new MenuItem(appMenuTemplate);
+  let menu = Menu.getApplicationMenu();
+  menu.append(appMenu);
+  Menu.setApplicationMenu(menu);
+}
+
+
 // This is the main window create function
-let mainWindowCreator = createKeygenWindow
+let mainWindowCreator = createMainWindow
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
